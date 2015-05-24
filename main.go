1afe066817a6724f0875ad3d1a0163dbe18cbbea
Chris Olsen <olsen.chris@gmail.com>
2015-05-24 11:51:49 -0600

    add redis caching

--- main.go
+++ main.go
@@ -2,23 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"time"
+
+	"menteslibres.net/gosexy/redis"
 )
 
+var rClient *redis.Client
+
 func main() {
+	// redis setup
+	rClient = redis.New()
+	if err := rClient.Connect("redis", 6379); err != nil {
+		log.Println("Unable to connect to redis")
+		return
+	}
+
 	http.HandleFunc("/", handleRoot)
 	http.ListenAndServe(":4200", nil)
 }
 
 func handleRoot(rw http.ResponseWriter, req *http.Request) {
-	t := time.Now()
+
+	answer, err := rClient.Get("answer2life")
+	if err == nil {
+		rw.Write(renderAnswer(answer))
+		return
+	}
+
 	time.Sleep(5 * time.Second)
 
-	template := `<div style="font-size:442px; text-align:center;">42</div>
+	rClient.Set("answer2life", "42")
+	rw.Write(renderAnswer("42"))
+}
+
+func renderAnswer(answer string) []byte {
+	t := time.Now()
+	template := `<div style="font-size:442px; text-align:center;">%v</div>
 	<em>Calculated at %v</em>`
 
-	content := []byte(fmt.Sprintf(template, t.String()))
-
-	rw.Write(content)
+	return []byte(fmt.Sprintf(template, answer, t.String()))
 }
